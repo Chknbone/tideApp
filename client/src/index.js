041e06ad608e0150ba6c77739c257fa880beeaf4
Michael <74569555+foleyartistbrown@users.noreply.github.com>
2021-01-27 18:07:52 -0800

    calendar app (#1)
    pop up on date when clicked
    
    Co-authored-by: abbykahler <70241972+abbykahler@users.noreply.github.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { render } from 'react-dom'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AlertTemplate from 'react-alert-template-basic'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +15,23 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
+    <App />
+  </AlertProvider>
+)
+ 
+render(<Root />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
